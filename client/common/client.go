f0209116efc3a7d034d9d7765e0ef92f1642ec9c
Siro-000 <sirofatala@gmail.com>
2025-08-31 20:16:44 +0000

    mas

--- client/common/client.go
+++ client/common/client.go
@@ -76,7 +76,7 @@ func (c *Client) StartClientLoop() {
 	c.listenSignals()
 	// There is an autoincremental msgID to identify every message sent
 	// Messages if the message amount threshold has not been surpassed
-	Loop:
+	log.Infof("Empieza el bucle")
 	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
 		select {
 		case <-c.ctx.Done():
