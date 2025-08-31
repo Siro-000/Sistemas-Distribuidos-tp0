f442a95c4ed48f846f2a40443f7ae22f57be0deb
Siro-000 <sirofatala@gmail.com>
2025-08-31 21:45:39 +0000

    se volvio

--- client/common/client.go
+++ client/common/client.go
@@ -10,8 +10,6 @@ import (
 )
 
 var log = logging.MustGetLogger("log")
-const WAIT_TIME = 1
-const BUFER_SIZE = 1 
 
 // ClientConfig Configuration used by the client
 type ClientConfig struct {
@@ -25,32 +23,15 @@ type ClientConfig struct {
 type Client struct {
 	config ClientConfig
 	conn   net.Conn
-	ctx    context.Context
-	cancel context.CancelFunc
 }
 
 // NewClient Initializes a new client receiving the configuration
 // as a parameter
 func NewClient(config ClientConfig) *Client {
-	ctx, cancel := context.WithCancel(context.Background())
-	return &Client{
+	client := &Client{
 		config: config,
-		ctx:    ctx,
-		cancel: cancel,
 	}
-}
-
-func (c *Client) listenSignals() {
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		sig := <-sigCh
-		log.Infof("Received signal %v, shutting down gracefully...", sig)
-		c.cancel()
-		if c.conn != nil {
-			c.conn.Close()
-		}
-	}()
+	return client
 }
 
 // CreateClientSocket Initializes client socket. In case of
@@ -64,50 +45,45 @@ func (c *Client) createClientSocket() error {
 			c.config.ID,
 			err,
 		)
-		return err
 	}
 	c.conn = conn
 	return nil
 }
 
-
 // StartClientLoop Send messages to the client until some time threshold is met
 func (c *Client) StartClientLoop() {
-	c.listenSignals()
 	// There is an autoincremental msgID to identify every message sent
 	// Messages if the message amount threshold has not been surpassed
-	log.Infof("\n\naction: se empieza el buvle | result: success | client_id: \n\n")
 	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
-		select {
-		case <-c.ctx.Done():
-			log.Infof("Salgo por aca")
-			log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
-			os.Exit(0) 
-		default:
-			
-			if err := c.createClientSocket(); err != nil {
-				return
-			}
+		// Create the connection the server in every loop iteration. Send an
+		c.createClientSocket()
 
-			// TODO: Modify the send to avoid short-write
-			fmt.Fprintf(c.conn, "[CLIENT %v] Message N°%v\n", c.config.ID, msgID)
+		// TODO: Modify the send to avoid short-write
+		fmt.Fprintf(
+			c.conn,
+			"[CLIENT %v] Message N°%v\n",
+			c.config.ID,
+			msgID,
+		)
+		msg, err := bufio.NewReader(c.conn).ReadString('\n')
+		c.conn.Close()
 
-			msg, err := bufio.NewReader(c.conn).ReadString('\n')
-			c.conn.Close()
-
-			if err != nil {
-				log.Infof("Salgo por error al recivir")
-				log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
-					c.config.ID, err)
-				return
-			}
-
-			log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
-				c.config.ID, msg)
-
-			time.Sleep(c.config.LoopPeriod)
+		if err != nil {
+			log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
+				c.config.ID,
+				err,
+			)
+			return
 		}
+
+		log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
+			c.config.ID,
+			msg,
+		)
+
+		// Wait a time between sending one message and the next one
+		time.Sleep(c.config.LoopPeriod)
+
 	}
-	log.Infof("Salgo por donde termina el loop")
 	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
 }
