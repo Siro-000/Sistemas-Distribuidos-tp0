8a3d0e4c72548b1f8d63157c98c87b09b3ccdf32
Siro-000 <sirofatala@gmail.com>
2025-08-31 02:54:31 +0000

    pequeño cambio

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -7,8 +7,10 @@ SERVER_PORT=$(grep "^SERVER_PORT" server/config.ini | cut -d '=' -f2 | tr -d '[:
 
 TEST_MSG="Hello"
 
+# obtener la red que Compose creó
+COMPOSE_NETWORK=$(docker network ls | grep testing_net | awk '{print $1}')
 
-RESULT=$(docker run --rm --network tp0_testing_net busybox sh -c "\
+RESULT=$(docker run --rm --network $COMPOSE_NETWORK busybox sh -c "\
   echo '$TEST_MSG' | nc $SERVER_CONTAINER $SERVER_PORT -w 2")
 
 # Comparamos la respuesta con el mensaje enviado
