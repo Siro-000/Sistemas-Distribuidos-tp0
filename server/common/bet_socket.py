e191832ab82bfb033913483e1033484bcc2ca1e6
Siro-000 <sirofatala@gmail.com>
2025-09-01 21:04:31 +0000

    manejro de errores

--- server/common/bet_socket.py
+++ server/common/bet_socket.py
@@ -1,8 +1,8 @@
-
 import json
 import struct
 
-from server.common.utils import Bet
+from .utils import Bet
+
 
 class IpMapAgenciNumber:
     def __init__(self):
@@ -15,42 +15,49 @@ class IpMapAgenciNumber:
             self.next_agency += 1
         return self.ip_to_agency[ip]
 
+
 class BetSocket:
     def __init__(self, socket):
         self._socket = socket
         self._ip_map_agenci_number = IpMapAgenciNumber()
     
-    def _recv_all(sock, n):
+    def _recv_all(self, n):
         """Lee exactamente n bytes o lanza error si la conexión se cierra antes."""
         data = b''
         while len(data) < n:
-            packet = sock.recv(n - len(data))
+            packet = self._socket.recv(n - len(data))
             if not packet:
                 raise ConnectionError("Socket cerrado antes de recibir todos los datos")
             data += packet
         return data
      
-    def recibe_bet(self) ->Bet:
-        raw_len = self.recv_all(self._socket, 4)
-        if not raw_len:
-            return
-        
+    def recibe_bet(self) -> Bet:
+        # Leer prefijo de 4 bytes (longitud del mensaje)
+        raw_len = self._recv_all(4)
         msg_len = struct.unpack('>I', raw_len)[0]
-        data = self.recv_all(self._socket, msg_len - len(data))
-        
-        bet = json.loads(data)
-        
+
+        # Leer mensaje completo
+        data = self._recv_all(msg_len)
+
+        # Parsear JSON
+        bet_json = json.loads(data)
+
+        # Crear objeto Bet
         return Bet(
-            agency=self._ip_map_agenci_number.get_agency_number(self._socket.getpeername()[0]),  
-            first_name=bet["nombre"],
-            last_name=bet["apellido"],
-            document=bet["dni"],
-            birthdate=bet["nacimiento"],
-            number=bet["numero"]
-            )
+            agency=self._ip_map_agenci_number.get_agency_number(
+                self._socket.getpeername()[0]
+            ),  
+            first_name=bet_json["nombre"],
+            last_name=bet_json["apellido"],
+            document=bet_json["dni"],
+            birthdate=bet_json["nacimiento"],
+            number=bet_json["numero"]
+        )
         
     def confirm(self): 
+        """Envía confirmación de 4 bytes cero al cliente."""
         self._socket.sendall(bytes(4))
     
     def close(self):
+        """Cierra la conexión del socket."""
         self._socket.close()
